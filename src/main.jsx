01fdce784a539e497862aee8ccab9afb0ec9bba7
Joel Grenon <joel@nukec.cloud>
2025-01-15 21:28:45 -0500

    add prod backend url variable

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider";
-import { AppRoutes } from "./components/ui/routes.jsx";
-import { BackendProvider } from "./components/backend-provider";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from './components/ui/provider';
+import { AppRoutes } from './components/ui/routes.jsx';
+import { BackendProvider } from './components/backend-provider';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <BackendProvider url="ws://localhost:8080">
+      <BackendProvider url={import.meta.env.VITE_BACKEND_URL}>
         <AppRoutes />
       </BackendProvider>
     </Provider>
